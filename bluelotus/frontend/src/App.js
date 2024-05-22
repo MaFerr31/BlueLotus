b8de27ba71e4cf0092bd96f1d805b3c237255e51
Mariana Santos Ferreira <marianasferra31@gmail.com>
2024-05-22 16:02:47 -0300

    Adicionando componente Sobre

--- bluelotus/frontend/src/App.js
+++ bluelotus/frontend/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Nav from "./Components/Nav/Nav";
 import GlobalStyle from "./styles/globals";
 import Home from "./Components/Home/Home";
+import Sobre from "./Components/Sobre/Sobre";
+import Footer from "./Components/Footer/Footer";
 
 
 const App=()=>{
@@ -14,6 +16,8 @@ const App=()=>{
                 <GlobalStyle/>
                 <Nav/>
                 <Home/> 
+                <Sobre/>
+                <Footer/>
                 
             </div>
         </div>
