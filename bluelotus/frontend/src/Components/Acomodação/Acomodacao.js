3c1bad1775072d6d51fece8b42f241218c651679
Mariana Santos Ferreira <marianasferra31@gmail.com>
2024-05-29 11:33:49 -0300

    Alteração visual no componente

--- bluelotus/frontend/src/Components/Acomodação/Acomodacao.js
+++ bluelotus/frontend/src/Components/Acomodação/Acomodacao.js
@@ -1,62 +1,86 @@
 import styles from "./Acomodacao.module.css";
+import Reserva from "./Reserva/Reserva";
 
 export default function Acomodacao() {
     return (
         <div className={styles["main"]}>
-            <h1>Nossos Quartos & Suítes</h1>
+            <h1>Acomodações</h1>
             <section className={styles["section-quarto"]}>
-                <div className={styles["box-main"]}>
-                    <img src="SUITE_DELUXE.png" alt="suitedeluxe" />
-                    <div className={styles["title-p"]}>
-                        <h2 className={styles["quarto-title"]}>suíte deluxe</h2>
-                        <p><span>$759</span>/por noite</p>
-                    </div>
-                    <div className={styles["lista-atributos"]}>
-                        <ul>
-                            <li>Wi-fi gratuito disponível</li>
-                            <li>Café da manhã incluso</li>
-                            <li>Cama King e sala de estar</li>
-                            <li>Até 3 pessoas</li>
-                        </ul>
-                        <button type="button" className={styles["btn-quarto"]}>reserve já</button>
-                    </div>
+                <div>
+                    <Reserva />
                 </div>
+                <div className={styles["box-main"]}>
+                    <div className={styles["option"]}><h2>Opções de quartos: </h2></div>
+                    <div className={styles["conteudo-quarto"]}>
+                        <img src="SUITE_DELUXE.png" alt="suitedeluxe" />
+                        <div>
+                            <h2 className={styles["title-quarto"]}> Suíte Deluxe</h2>
+                            <p> Cama Queen | 28 m² | 3 hóspedes</p>
+                            <ul>
+                                <li>Wi-fi grátis</li>
+                                <li>Ar condicionado</li>
+                                <li>Serviço de quarto</li>
+                                <li>Café da manhã</li>
+                            </ul>
+                        </div>
+                        <span> <strong>Especificações</strong>
+                            <br />
+                            Tipo: 1
+                            <br />
+                            Check In: antes 4pm
+                            <br /> Check out: Antes 12pm</span>
+                        <h3>Valor: <strong className={styles["valor-strong"]}>$645,00</strong> a diária</h3>
+                    </div>
+
+                    <div className={styles["conteudo-quarto"]}>
+                        <img src="IMG_SUITE_DUPLA.png" alt="suitedeluxe" />
+                        <div>
+                            <h2 className={styles["title-quarto"]}> Suíte Deluxe Dupla</h2>
+                            <p> Cama King | 30 m² | 4 hóspedes</p>
+                            <ul>
+                                <li>Wi-fi grátis</li>
+                                <li>Ar condicionado</li>
+                                <li>Serviço de quarto</li>
+                                <li>Café da manhã</li>
+                            </ul>
+                        </div>
+                        <span> <strong>Especificações</strong>
+                            <br />
+                            Tipo: 2
+                            <br />
+                            Check In: antes 4pm
+                            <br /> Check out: Antes 12pm</span>
+                        <h3>Valor: <strong className={styles["valor-strong"]}>$789,00</strong> a diária </h3>
+                    </div>
+
+                    <div className={styles["conteudo-quarto"]}>
+                        <img src="SUITE_GRANDE.png" alt="suitedeluxe" />
+                        <div>
+                            <h2 className={styles["title-quarto"]}> Suíte Grande</h2>
+                            <p> Cama Casal | 21 m² | 2 hóspedes</p>
+                            <ul>
+                                <li>Wi-fi grátis</li>
+                                <li>Ar condicionado</li>
+                                <li>Serviço de quarto</li>
+                                <li>Café da manhã</li>
+                            </ul>
+                        </div>
+                        <span> <strong>Especificações</strong>
+                            <br />
+                            Tipo: 3
+                            <br />
+                            Check In: antes 4pm
+                            <br /> Check out: Antes 12pm</span>
+                        <h3>Valor: <strong className={styles["valor-strong"]}>$569,00</strong> a diária</h3>
+
+                    </div>
 
 
-                <div className={styles["box-main"]}>
-                    <img src="IMG_SUITE_DUPLA.png" alt="suitedeluxe" />
-                    <div className={styles["title-p"]}>
-                        <h2 className={styles["quarto-title"]}>suíte dupla</h2>
-                        <p><span>$930</span>/por noite</p>
-                    </div>
-                    <div className={styles["lista-atributos"]}>
-                        <ul>
-                            <li>Wi-fi exclusivo disponível</li>
-                            <li>Café da manhã incluso</li>
-                            <li>Duas camas Queen</li>
-                            <li>Até 4 pessoas</li>
-                        </ul>
-                        <button type="button" className={styles["btn-quarto"]}>reserve já</button>
-                    </div>
                 </div>
 
-                <div className={styles["box-main"]}>
-                    <div className={styles["img"]}><img src="SUITE_GRANDE.png" alt="suitedeluxe" /></div>
-                    <div className={styles["title-p"]}>
-                        <h2 className={styles["quarto-title"]}>suíte grande</h2>
-                        <p><span>$589</span>/por noite</p>
-                    </div>
-                    <div className={styles["lista-atributos"]}>
-                        <ul>
-                            <li>Wi-fi gratuito disponível</li>
-                            <li>Café da manhã incluso</li>
-                            <li>Uma cama Queen</li>
-                            <li>Até 2 pessoas</li>
-                        </ul>
-                        <button type="button" className={styles["btn-quarto"]}>reserve já</button>
-                    </div>
-                </div>
             </section>
+
+
 
         </div>
     )
