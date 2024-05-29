35cd2805bb4e503e751a6c0563769833b278bcba
Mariana Santos Ferreira <marianasferra31@gmail.com>
2024-05-29 11:35:50 -0300

    Criação de componente Imagens

---
+++ bluelotus/frontend/src/Components/Seq-Imagens/Imagens.js
@@ -0,0 +1,13 @@
+import styles from "./Imagens.module.css";
+
+export default function Imagens()  {
+    return (
+       <div className={styles["img-rodape"]}> 
+        <img src="IMG-7.jpg" alt="Img"></img>
+        <img src="IMG-4.jpg" alt="Img"></img>
+        <img src="IMG-2.jpg" alt="Img"></img>
+        <img src="IMG-5.jpg" alt="Img"></img>
+        <img src="IMG-6.jpg" alt="Img"></img>
+       </div>
+    )
+}
