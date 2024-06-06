b02b98a52c8f51e34cca17c2d2e7a9297a94c7e3
Mariana Santos Ferreira <marianasferra31@gmail.com>
2024-06-06 10:33:22 -0300

    Modificação navegação

--- bluelotus/frontend/src/Components/Nav/Nav.js
+++ bluelotus/frontend/src/Components/Nav/Nav.js
@@ -9,14 +9,13 @@ export default function Nav() {
                 <nav className={styles['nav_menu']}>
                     <ul className={styles['nav_lista']}>
                         <li><NavLink to ="/home">Home</NavLink></li>
-                        <li><NavLink to ="/sobre">Sobre</NavLink></li>
+                        <li><NavLink to ="/perfil">perfil</NavLink></li>
                         <li><NavLink to ="/acomodacoes">acomodações</NavLink></li>
-                        <li><NavLink to ='/reservas'>reservas</NavLink></li>
-                        <li><a href='/contato'>contato</a></li>
+                        <li><NavLink to ='/sobre'>Sobre</NavLink></li>
                     </ul>
                 </nav>
                 <div className={styles["icons"]}>
-                    <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <NavLink to ="/login"><svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M30 32.5C34.1421 32.5 37.5 29.1421 37.5 25C37.5 20.8579 34.1421 17.5 30 17.5C25.8579 17.5 22.5 20.8579 22.5 25C22.5 29.1421 25.8579 32.5 30 32.5Z"
                             stroke="#222222" stroke-width="2.5" stroke-linecap="round" />
@@ -26,7 +25,7 @@ export default function Nav() {
                         <path
                             d="M45 46.7648C44.1153 44.1068 42.1655 41.7582 39.4535 40.083C36.7415 38.408 33.4185 37.5 30 37.5C26.5815 37.5 23.2585 38.408 20.5465 40.083C17.8344 41.7582 15.8848 44.1068 15 46.7648"
                             stroke="#222222" stroke-width="2.5" stroke-linecap="round" />
-                    </svg>
+                    </svg></NavLink>
 
                 </div>
             </header>
