d28ce4d6ca503783c6a8d50e56a9c69e0332e2c7
Mariana Santos Ferreira <marianasferra31@gmail.com>
2024-06-06 10:33:01 -0300

    Cadastro

--- bluelotus/frontend/src/Components/Cadastro/Cadastro.js
+++ bluelotus/frontend/src/Components/Cadastro/Cadastro.js
@@ -1,59 +1,78 @@
-import React, { useState } from "react";
-import axios from "axios";
-import styles from './Cadastro.module.css'
+import React, { useState } from 'react';
+import styles from './Cadastro.module.css';
+import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 
 const Cadastro = () => {
 
-  const [nome, setNome] = useState('');
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
-  const [mensagem, setMensagem] = useState('');
-
-  const handleCadastro = async (e) => {
-    e.preventDefault();
-
-    try {
-      await axios.post('http://localhost:4000/usuario', {
-          nome, email, senha 
-      });
-
-      setMensagem('Usuário criado com sucesso!')
-    } catch (error) {
-      if (error.response && error.response.status === 400) {
-        setMensagem("Credenciais invalidas!")
-      } else {
-        setMensagem("Falha ao realizar o cadastro")
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [mensagem, setMensagem] = useState('');
+  
+    const handleCadastro = async (e) => {
+      e.preventDefault();
+  
+      try {
+        await axios.post('http://localhost:4000/usuario', {
+            nome, email, senha 
+        });
+  
+        setMensagem('Cadastro realizado com sucesso!')
+      } catch (error) {
+        if (error.response && error.response.status === 400) {
+          setMensagem("Informações inválidas!")
+        } else {
+          setMensagem("Falha ao realizar o cadastro")
+        }
+        console.log(error)
       }
-      console.log(error)
-    }
-  };
-
+    };
+   
   return (
-
-    <div className={styles['container']}>
-
-      <form onSubmit={handleCadastro} className={styles["form-login"]}>
-        <div>
-          <h1 className={styles["title-login"]}>Cadastre-se</h1>
-          <div className={styles["input-class"]}>
-            <input type="text" placeholder="Nome" onChange={(e) => setNome(e.target.value)} value={nome.nome} required/>
+    <div className={styles["container"]}>
+      <div className={styles["form-image"]}>
+        <img src="IMG-9.jpg" alt="" />
+      </div>
+      <div className={styles["form"]}>
+        <form onSubmit={handleCadastro}>
+          <div className={styles["form-header"]}>
+            <div className={styles["title"]}>
+              <h1>Cadastre-se</h1>
+            </div>
           </div>
 
-          <div className={styles["input-class"]}>
-            <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} value={email.email} required/>
+          <div className={styles["input-group"]}>
+            <div className={styles["input-box"]}>
+              <label className={styles["label-cds"]}htmlFor="firstname">Nome Completo:</label>
+              <input id="firstname" type="text" name="firstname" onChange={(e) => setNome(e.target.value)} value={nome.nome} required />
+            </div>
+
+            <div className={styles["input-box"]}>
+              <label className={styles["label-cds"]}htmlFor="email">E-mail:</label>
+              <input id="email" type="email" name="email" onChange={(e) => setEmail(e.target.value)} value={email.email} required />
+            </div>
+
+            <div className={styles["input-box"]}>
+              <label className={styles["label-cds"]}htmlFor="password">Senha:</label>
+              <input id="password" type="password" name="password" onChange={(e) => setSenha(e.target.value)} value={senha.senha} required />
+            </div>
+
+            <div className={styles["input-box"]}>
+              <label className={styles["label-cds"]} htmlFor="confirmPassword">Confirme sua senha: </label>
+              <input id="confirmPassword" type="password" name="confirmPassword"  onChange={(e) => setSenha(e.target.value)} value={senha.senha} required />
+            </div>
           </div>
-          <div className={styles["input-class"]}>
-            <input type="password" placeholder="Senha" onChange={(e) => setSenha(e.target.value)} value={senha.senha} required/>
+          <div className={styles["mensagem"]}>{mensagem}</div>
+          <div className={styles["continue-button"]}>
+            <button><NavLink to = "/home">Voltar</NavLink></button>
+            <button>Cadastrar</button>
           </div>
-        </div>
-        <p className={styles["mensagem"]}>{mensagem}</p>
-        <div className={styles["bnt-login"]}>
-          <button type="submit" className={styles["bnt"]}>Confirmar</button>
-        </div>
-      </form>
+         
+        </form>
+      </div>
     </div>
   );
-
-};
+}
 
 export default Cadastro;
